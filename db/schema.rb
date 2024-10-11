1276a4ef50b4cd538fc8aac777686d1ccec1c86e
Renee Messersmith <morganrmessersmith@gmail.com>
2024-10-11 15:48:45 -0400

    Fix: removed the api_key column from parties

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_11_193234) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_11_194652) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_11_193234) do
     t.datetime "start_time"
     t.datetime "end_time"
     t.string "movie_title"
-    t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "movie_id"
