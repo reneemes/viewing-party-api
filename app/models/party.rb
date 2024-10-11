2dc16b4256d8f510ed3b5486dbc4b88e9d9af6e2
Renee Messersmith <morganrmessersmith@gmail.com>
2024-10-11 16:49:41 -0400

    Test: relationships are propperly tested

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,12 @@
 class Party < ApplicationRecord
-  belongs_to :host, class_name: 'User', foreign_key: 'host_id'
+  # belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   has_many :user_parties
   has_many :users, through: :user_parties
+
+  validates :name, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :movie_id, presence: true
+  validates :movie_title, presence: true
+  # validates :api_key, presence: { require: true }
 end
--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -2,17 +2,18 @@ require "rails_helper"
 
 RSpec.describe Party, type: :model do
   describe "validations" do
-    # it { should validate_presence_of(:name) }
-    # it { should validate_presence_of(:username) }
-    # it { should validate_uniqueness_of(:username) }
-    # it { should validate_presence_of(:password) }
-    # it { should have_secure_password }
-    # it { should have_secure_token(:api_key) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:start_time) }
+    it { should validate_presence_of(:end_time) }
+
+    it { should validate_presence_of(:movie_id) }
+    it { should validate_presence_of(:movie_title) }
+    # it { should validate_presence_of(:api_key) }
   end
 
   describe "relationships" do
     it { should have_many :user_parties }
     it { should have_many(:users).through(:user_parties) }
-    it { should belong_to(:user).with_foreign_key('host_id') }
+    # it { should belong_to(:user).with_foreign_key('user_id') }
   end
 end
