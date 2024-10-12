f13ab550c5707cb90b50ab25999cb1c8558ee554
Renee Messersmith <morganrmessersmith@gmail.com>
2024-10-12 17:49:02 -0400

    Test: party model methods

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe Party, type: :model do
     it { should have_many(:users).through(:user_parties) }
     # it { should belong_to(:user).with_foreign_key('user_id') }
   end
+
+  it "checks the party time is valid" do
+    dolly = User.create!(name: "Dolly Parton", username: "dollyP", password: "Jolene123")
+
+    expect(Party.all.count).to eq(0)
+
+    party = Party.create(
+      name: "Movie Party",
+      start_time: "2025-02-01 14:30:00",
+      end_time: "2025-02-01 10:00:00",
+      movie_id: 278,
+      movie_title: "The Shawshank Redemption"
+      )
+    
+    expect(Party.all.count).to eq(0)
+  end
 end
