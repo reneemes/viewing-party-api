ed034aac3db007506c2c1be3aa4df4dd31b87963
Renee Messersmith <morganrmessersmith@gmail.com>
2024-10-11 21:10:29 -0400

    Feat: ITS WORKING

--- app/controllers/api/v1/parties_controller.rb
+++ app/controllers/api/v1/parties_controller.rb
@@ -1,15 +1,21 @@
 class Api::V1::PartiesController < ApplicationController
-  before_action :verify_api_key, only: :create
+  before_action :verify_api_key#, only: :create
 
   def create
     # use the api key to validate the user and also find the user
     # use the invitees to create new instances on the user_parties table
+    user = User.find_by(api_key: params[:api_key])
+    if user.nil?
+      return render json: { error: 'No user found' }, status: :not_found
+    end
 
     new_party = Party.create(party_params)
-    if new_party.save
+    if new_party#.save
       render json: PartySerializer.new(new_party), status: :created
+      handle_invitees(params[:invitees], new_party, user)
     else
       # do some error handling here
+      return render json: { errors: new_party.errors.full_message }, status: :unprocessable_entity
     end
   end
 
@@ -21,7 +27,15 @@ class Api::V1::PartiesController < ApplicationController
 
   def verify_api_key
     if params[:api_key].blank?
-      render json: { error: 'Invalid or missing API key' }, status: :unauthorized
+      return render json: { error: 'Invalid or missing API key' }, status: :unauthorized
     end
   end
+
+  def handle_invitees(invitees, party, host)
+    UserParty.create(user_id: host.id, party_id: party.id, is_host: true)
+    invitees.each do |user_id|
+      UserParty.create(user_id: user_id, party_id: party.id, is_host: false)
+    end
+  end
+
 end
